157e53fdd88a1838fcc2b63fd8f07ab7ab2b0ec2
Seamus Narron <seamus.narron@gmail.com>
2018-11-26 16:58:58 -0500

    Integrated edit and update action for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
-  
+
   def new
     @portfolio_item = Portfolio.new
   end
-  
+
   def create
     @portfolio_item = Portfolio.new(
       params.require(:portfolio).permit(:title, :subtitle, :body)
     )
-    
+
     respond_to do |format|
       if @portfolio_item.save
         format.html do
@@ -22,4 +24,26 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+  
+    respond_to do |format|
+      if @portfolio_item.update(
+        params.require(:portfolio).permit(:title, :subtitle, :body)
+      )
+        format.html do
+          redirect_to portfolios_path,
+                      notice: 'The record successfully updated.'
+        end
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
 end

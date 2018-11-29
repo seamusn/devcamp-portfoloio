fab30ed3e4ffaba0d5488bb6371e6a273eff9f7a
Seamus Narron <seamus@uga.edu>
2018-11-29 16:13:23 -0500

    Implemented destroy action for portfolio feature

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@
   )
 end
 
-puts "10 blog posts created"
+puts '10 blog posts created'
 
 5.times do |skill|
   Skill.create!(
@@ -29,24 +29,22 @@ puts "10 blog posts created"
   )
 end
 
-puts "5 skills created"
+puts '5 skills created'
 
 9.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title #{portfolio_item}",
-    subtitle: "My Great service",
-    body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    subtitle: 'My Great service',
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
       eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
       minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
       ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
       sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
       mollit anim id est laborum.",
-    main_image: "https://via.placeholder.com/600x400",
-    thumb_image: "https://via.placeholder.com/350x200",
-    
+    main_image: 'https://via.placeholder.com/600x400',
+    thumb_image: 'https://via.placeholder.com/350x200'
   )
 end
 
-puts "9 portfolios items created"
-
+puts '9 portfolios items created'

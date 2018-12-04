1a1c4aa768b319bc2df56df9c1b5a6fea05647c5
Seamus Narron <seamus@uga.edu>
2018-12-04 14:31:29 -0500

    Integrated form configuration for nested attributes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -11,11 +11,13 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end
 
   def create
     @portfolio_item = Portfolio.new(
-      params.require(:portfolio).permit(:title, :subtitle, :body)
+      params.require(:portfolio).permit(:title, :subtitle, :body,
+                                        technologies_attributes: [:name])
     )
 
     respond_to do |format|

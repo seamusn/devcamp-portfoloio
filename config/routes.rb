26b31b52a19a394be96a5aa41065d905886c658d
Seamus Narron <seamus@uga.edu>
2018-12-10 13:25:59 -0500

    Customized registration, login, logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path:'', path_names: {sign_in: 'login',sign_out:
+    'logout',sign_up: 'register' }
   resources :portfolios, except: [:show]
 
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'

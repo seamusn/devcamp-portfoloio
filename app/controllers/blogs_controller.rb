b1eac9acc916310c480276d94a91d81197082213
Seamus Narron <seamus@uga.edu>
2018-12-12 15:28:10 -0500

    Added nav bar and default header content

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,11 +7,15 @@ class BlogsController < ApplicationController
   # GET /blogs.json
   def index
     @blogs = Blog.all
+    @page_title = 'My Portfolio Blog'
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
-  def show; end
+  def show
+    @page_title = @blog.title
+    @seo_keywords = @blog.body
+  end
 
   # GET /blogs/new
   def new

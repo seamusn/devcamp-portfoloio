e65c6b7a0cc181adf7196375f86d5302ad236b8f
Seamus Narron <seamus@uga.edu>
2018-12-20 14:28:49 -0500

    Implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+#frozen_string_literal: false
 
 module ApplicationHelper
   def login_helper style = ''
@@ -23,6 +23,43 @@ module ApplicationHelper
 
   def copyright_generator
     NarronViewTool::Renderer.copyright 'Seamus Narron', 'All rights reserved'
+  end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_path,
+        title: 'About'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      }
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
   end
 
 end

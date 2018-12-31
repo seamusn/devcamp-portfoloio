91afc392b99bc8fa0695b06d803f1fe6d2bf48f6
Seamus Narron <seamus@uga.edu>
2018-12-31 15:40:28 -0500

    Implemented initial file uploading system via carrierwave for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,9 +5,13 @@ class Portfolio < ApplicationRecord
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda{ |attrs| attrs['name']
                                   .blank? }
+
   include Placeholder
 
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
 
   def self.angular
     where(subtitle: 'Angular')

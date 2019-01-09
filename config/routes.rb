913095e03f98c5ecc1edab09fb8a7ab967c1d465
Seamus Narron <seamus@uga.edu>
2019-01-09 11:19:20 -0500

    Created a Comment broadcast, configured the websocket route, updated the comment model with a callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out:
     'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do
@@ -22,7 +21,7 @@ Rails.application.routes.draw do
     end
   end
 
-
+  mount ActionCable.server => '/cable'
 
   root to: 'pages#home'
 end

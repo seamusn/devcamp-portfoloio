30dd447b269fabee0cfb8f74c26c247369654334
Seamus Narron <seamus@uga.edu>
2019-01-11 13:38:33 -0500

    Built out topic views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :topics, only: [:index,:show]
+
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out:
     'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do

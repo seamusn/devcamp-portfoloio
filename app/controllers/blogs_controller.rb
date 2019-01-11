400d3ad3e87a7e645d24469b22ebbdf832fb78d3
Seamus Narron <seamus@uga.edu>
2019-01-11 14:10:05 -0500

    Implemented blog widget for topics

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,6 +2,7 @@
 
 class BlogsController < ApplicationController
   before_action :set_blog, only: %i[show edit update destroy toggle_status]
+  before_action :set_sidebar_topics, except: [:update, :create, :destroy, :toggle_status]
   layout 'blog'
   access all: [:show, :index],
          user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]},
@@ -106,5 +107,9 @@ class BlogsController < ApplicationController
   # only allow the white list through.
   def blog_params
     params.require(:blog).permit(:title, :body, :topic_id)
+  end
+
+  def set_sidebar_topics
+    @side_bar_topics = Topic.with_blogs
   end
 end
